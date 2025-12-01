a3823acd5ab08a2cdcc9bd45c5fc3aa75e3b22ea
Kimsuro <ss01056489656@gmail.com>
2025-12-01 20:11:36 +0900

    style: 비밀번호 찾기 페이지 디자인 구현

--- src/pages/forget-password-page.tsx
+++ src/pages/forget-password-page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function ForgetPasswordPage() {
-  return <div>ForgetPasswordPage</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-1">
+        <h2 className="text-xl font-bold">비밀번호를 잊으셨나요?</h2>
+        <div className="text-muted-foreground text-sm">
+          이메일로 비밀번호를 재설정 할 수 있는 인증 링크를 보내드립니다.
+        </div>
+      </div>
+
+      <Input className="py-6" placeholder="example@abc.com" />
+      <Button className="w-full">인증 코드 발송</Button>
+    </div>
+  );
 }

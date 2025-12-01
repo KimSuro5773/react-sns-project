26f46ff52f72e25aa642c4751600b83334ff67a9
Kimsuro <ss01056489656@gmail.com>
2025-12-01 20:13:36 +0900

    style: 비밀번호 재설정 페이지 디자인 구현

--- src/pages/reset-password-page.tsx
+++ src/pages/reset-password-page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function ResetPasswordPage() {
-  return <div>ResetPasswordPage</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-1">
+        <h2 className="text-xl font-bold">비밀번호 재설정하기</h2>
+        <div className="text-muted-foreground text-sm">
+          새로운 비밀번호를 입력하세요
+        </div>
+      </div>
+
+      <Input className="py-6" type="password" placeholder="password" />
+      <Button className="w-full">비밀번호 변경</Button>
+    </div>
+  );
 }

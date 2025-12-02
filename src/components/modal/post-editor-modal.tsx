e62faf565760f76c5c77647c4e91b7d8529d3706
Kimsuro <ss01056489656@gmail.com>
2025-12-02 20:10:51 +0900

    feat: 포스트 생성 버튼 클릭시 모달이 나오는 기능 구현

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -1,10 +1,17 @@
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { ImageIcon } from "lucide-react";
+import { usePostEditorModal } from "@/store/post-editor-modal";
 
 export default function PostEditorModal() {
+  const { isOpen, close } = usePostEditorModal();
+
+  const handleCloseModal = () => {
+    close();
+  };
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={handleCloseModal}>
       <DialogContent>
         <DialogTitle>포스트 작성</DialogTitle>
         <textarea></textarea>

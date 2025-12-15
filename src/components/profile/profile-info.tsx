b614644039617ce70393d9d13215980d01705752
Kimsuro <ss01056489656@gmail.com>
2025-12-15 22:33:40 +0900

    feat: 프로필 수정 모달 UI 구현
    본인 프로필 페이지에서만 보이는 프로필 수정 버튼을 통해 수정 모달을 열 수 있는 버튼과 모달 UI를 구현했습니다.
    - ProfileEditorModal 컴포넌트 구현 (UI만 구현)
    - EditProfileButton 컴포넌트 구현
    - profile-editor-modal 스토어 추가

--- src/components/profile/profile-info.tsx
+++ src/components/profile/profile-info.tsx
@@ -2,8 +2,12 @@ import { useProfileData } from "@/hooks/queries/use-profile-data";
 import FallBack from "@/components/fallback";
 import Loader from "@/components/loader";
 import defaultAvatar from "@/assets/default-avatar.jpg";
+import { useSession } from "@/store/session";
+import EditProfileButton from "@/components/profile/edit-profile-button";
 
 export default function ProfileInfo({ userId }: { userId: string }) {
+  const session = useSession();
+
   const {
     data: profile,
     error: fetchProfileError,
@@ -12,6 +16,8 @@ export default function ProfileInfo({ userId }: { userId: string }) {
 
   if (fetchProfileError) return <FallBack />;
   if (isFetchingProfilePending) return <Loader />;
+
+  const isMine = session?.user.id === userId;
 
   return (
     <div className="flex flex-col items-center justify-center gap-5">
@@ -24,6 +30,7 @@ export default function ProfileInfo({ userId }: { userId: string }) {
         <div className="text-xl font-bold">{profile.nickname}</div>
         <div className="text-muted-foreground">{profile.bio}</div>
       </div>
+      {isMine && <EditProfileButton />}
     </div>
   );
 }

6f3218ed56126961e452a584f5e0a03b4bcd0134
Kimsuro <ss01056489656@gmail.com>
2025-12-19 22:24:20 +0900

    feat: 댓글 목록 조회 기능 구현
    - fetchComments API 함수 추가 (작성자 정보 포함)
    - useCommentsData 커스텀 훅 생성
    - Comment 타입 정의 (CommentEntity + ProfileEntity)

--- src/components/comment/comment-list.tsx
+++ src/components/comment/comment-list.tsx
@@ -1,11 +1,23 @@
 import CommentItem from "@/components/comment/comment-item";
+import { useCommentsData } from "@/hooks/queries/use-comments-data";
+import FallBack from "@/components/fallback";
+import Loader from "@/components/loader";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentsError,
+    isPending: isFetchCommentsPending,
+  } = useCommentsData(postId);
+
+  if (fetchCommentsError) return <FallBack />;
+  if (isFetchCommentsPending) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments.map((comments) => (
+        <CommentItem key={comments.id} {...comments} />
+      ))}
     </div>
   );
 }

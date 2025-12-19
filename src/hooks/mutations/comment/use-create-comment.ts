e1c168d2eab3fb0cbf398e8e11cdfd9532b28128
Kimsuro <ss01056489656@gmail.com>
2025-12-19 23:17:41 +0900

    refactor: 댓글 CRUD 캐시 데이터 조작으로 UI가 변경되도록 개선
    - useCreateComment: 댓글 생성 시 즉시 케시에 추가하여 UI 반영
    - useUpdateComment: 댓글 수정 시 캐시 데이터 즉시 업데이트
    - useDeleteComment: 댓글 삭제 시 캐시에서 필터링하여 제거
    - queryClient.setQueryData로 로컬 캐시 업데이트

--- src/hooks/mutations/comment/use-create-comment.ts
+++ src/hooks/mutations/comment/use-create-comment.ts
@@ -1,12 +1,32 @@
 import { createComment } from "@/api/comment";
-import type { UseMutationCallback } from "@/types";
-import { useMutation } from "@tanstack/react-query";
+import { useProfileData } from "@/hooks/queries/use-profile-data";
+import { QUERY_KEYS } from "@/lib/constants";
+import { useSession } from "@/store/session";
+import type { Comment, UseMutationCallback } from "@/types";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateComment(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+  const session = useSession();
+  const { data: propfile } = useProfileData(session?.user.id);
+
   return useMutation({
     mutationFn: createComment,
-    onSuccess: () => {
+    onSuccess: (newComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(newComment.post_id),
+        (comments) => {
+          if (!comments)
+            throw new Error("댓글이 데이터에 보관되어있지 않습니다.");
+
+          if (!propfile)
+            throw new Error("사용자의 프로필 정보를 찾을 수 없습니다.");
+
+          return [{ ...newComment, author: propfile }, ...comments];
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
